465790f71b25b60f5ed782ba51eeb42098ebf50c
KyunyeonKim <kgn4746@naver.com>
2024-07-17 13:43:19 +0900

    변경

--- TEST.sql
+++ TEST.sql
@@ -1,1 +1,18 @@
-select * from tab;
+ALTER SESSION SET CURRENT_SCHEMA = HR;
+
+SELECT
+    *
+FROM departments;
+
+
+
+SELECT employee_id , salary
+    
+FROM employees order by salary;
+
+--사원테이블에서 업무코드와 급여 출력
+SELECT
+    job_id,salary
+FROM employees
+ORDER BY job_id DESC, salary desc;
+
